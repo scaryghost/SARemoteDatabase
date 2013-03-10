d4fac409e8494eaf07e55eef3c4da4f849ebc9e6
etsai <etsai2@gmail.com>
2013-03-10 15:01:17 -0700

    Setup logging and expanded command line options
    Updated CppUtilities submodule to the fixed version of  ConsoleHandler

--- Main/src/SARemoteDatabase.cpp
+++ Main/src/SARemoteDatabase.cpp
@@ -1,4 +1,5 @@
 #include "CppUtilities/Cli/cli.h"
+#include "CppUtilities/Logging/logging.h"
 #include "CppUtilities/Network/ServerSocket.h"
 #include "CppUtilities/Network/Socket.h"
 
@@ -6,8 +7,8 @@
 #include "Database/src/SqliteConnection.h"
 
 #include <cstdlib>
-#include <iostream>
 #include <memory>
+#include <sstream>
 #include <thread>
 
 using namespace etsai::cpputilities;
@@ -15,15 +16,19 @@ using namespace etsai::saremotedatabase;
 using namespace std;
 
 Connection *dbConn;
+Logger *logger;
 
 void handler(shared_ptr<Socket> socket) {
-    string line;
-    cout << "request: " << socket->readLine() << endl;
+    string line, data;
+    
+    logger->log(Level::INFO, "request: " + socket->readLine());
     socket->write("server-achievements,1,response|0");
 
     while((line= socket->readLine()) != "") {
-        cout << line << endl;
-        socket->write("server-achievements,1,response|0|" + dbConn->retrieveAchievementData("12345","blah"));
+        logger->log(Level::INFO, line);
+        data= "server-achievements,1,response|0|" + dbConn->retrieveAchievementData("76561197961630515","Stock KF");
+        socket->write(data);
+        logger->log(Level::INFO, "data: " + data);
     }
 }
 
@@ -33,28 +38,44 @@ void initDbConnection(const string& dbURL, const string& user, const string& pas
 }
 
 int main(int argc, char **argv) {
+    logger= Logger::getLogger("saremotedatabase");
     ServerSocket server;
     int port;
-    string dbURL;
+    string dbURL, dbUser, dbPasswd;
 
     CliBuilder *cli= CliBuilder::getBuilder();
     (*cli).addOption(Option("-tcpport", 0, 1, [&port](const Arguments &args) -> void {
         port= args.asInteger(0);
     }).withArgName("number").withDescription("TPC port to listen on for requests").withRequired(true))
-    .addOption(Option("-database", 0, 1, [&dbURL](const Arguments &args) -> void {
+    .addOption(Option("-dburl", 0, 1, [&dbURL](const Arguments &args) -> void {
         dbURL= args.asString(0);
-    }).withArgName("url").withDescription("Database storing achievement information").withRequired(true))
+    }).withArgName("url").withDescription("URL to the database storing achievement information").withRequired(true))
     .addOption(Option("-h", [&cli](const Arguments &args) -> void {
         cli->displayUsage();
     }).withLongOpt("--help").withDescription("Displays this help message and exits"))
+    .addOption(Option("-dbuser", 0, 1, [&dbUser](const Arguments &args) -> void {
+        dbUser= args.asString(0);
+    }).withArgName("user name").withDescription("Username for logging into the database"))
+    .addOption(Option("-dbpwd", 0, 1, [&dbPasswd](const Arguments &args) -> void {
+        dbPasswd= args.asString(0);
+    }).withArgName("password").withDescription("Password for logging into the database"))
     .setUsage("saremotedatabase -database <name> -tcpport <number> [options]");
     cli->parse(argc, argv);
 
+    logger->addHandler(new ConsoleHandler());
+    logger->addHandler(new FileHandler());
     server.bind(port);
-    cout << "Listening on port: " << port << endl;
+    initDbConnection(dbURL, dbUser, dbPasswd);
+    {
+        stringstream msg;
+        msg << "Listening on port: " << port;
+        logger->log(Level::INFO, msg.str());
+    }
     while(true) {
+        stringstream msg;
         shared_ptr<Socket> client= server.accept();
-        cout << "Received connection from " << client->getAddress() << ":" << client->getPort() << endl;
+        msg << "Received connection from " << client->getAddress() << ":" << client->getPort();
+        logger->log(Level::INFO, msg.str());
         thread th(handler, client);
         th.detach();
     }
