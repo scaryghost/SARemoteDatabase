241361e4f3bf9b11b245525cd152bd3eb887047c
etsai <etsai2@gmail.com>
2013-03-10 22:16:30 -0700

    Added password checking and updated submodule

--- Main/src/SARemoteDatabase.cpp
+++ Main/src/SARemoteDatabase.cpp
@@ -20,21 +20,81 @@ using namespace std;
 Connection *dbConn;
 Logger *logger;
 
+static int port;
+static string dbURL, dbUser, dbPasswd, password;
+
+void start();
+void handler(shared_ptr<Socket> socket);
+void initDbConnection();
+
+int main(int argc, char **argv) {
+    logger= Logger::getLogger("saremotedatabase");
+
+    CliBuilder *cli= CliBuilder::getBuilder();
+    (*cli).addOption(Option("-tcpport", 0, 1, [](const Arguments &args) -> void {
+        port= args.asInteger(0);
+    }).withArgName("number").withDescription("TCP port to listen on for requests").withRequired(true))
+    .addOption(Option("-dburl", 0, 1, [](const Arguments &args) -> void {
+        dbURL= args.asString(0);
+    }).withArgName("url").withDescription("URL to the database storing achievement information").withRequired(true))
+    .addOption(Option("-passwd", 0, 1, [](const Arguments &args) -> void {
+        password= args.asString(0);
+    }).withArgName("value").withDescription("Password for connecting to the server"))
+    .addOption(Option("-h", [&cli](const Arguments &args) -> void {
+        cli->displayUsage();
+    }).withLongOpt("--help").withDescription("Displays this help message and exits"))
+    .addOption(Option("-dbuser", 0, 1, [](const Arguments &args) -> void {
+        dbUser= args.asString(0);
+    }).withArgName("user name").withDescription("Username for logging into the database"))
+    .addOption(Option("-dbpwd", 0, 1, [](const Arguments &args) -> void {
+        dbPasswd= args.asString(0);
+    }).withArgName("password").withDescription("Password for logging into the database"))
+    .setUsage("saremotedatabase -dburl <url> -tcpport <number> [options]");
+    cli->parse(argc, argv);
+
+    logger->addHandler(new ConsoleHandler());
+    logger->addHandler(new FileHandler());
+    initDbConnection();
+    start();
+
+    return 0;
+}
+
+void start() {
+    ServerSocket server;
+    stringstream msg;
+
+    msg << "Listening on port: " << port;
+    logger->log(Level::INFO, msg.str());
+    server.bind(port);
+    while(true) {
+        shared_ptr<Socket> client= server.accept();
+        logger->log(Level::INFO, "Received connection from " + client->getAddressPort());
+        thread th(handler, client);
+        th.detach();
+    }
+}
+
 void handler(shared_ptr<Socket> socket) {
+    bool validConnection= true;
     string line;
     
-    logger->log(Level::INFO, "request: " + socket->readLine());
-    socket->write("server-achievements,1,response|0");
-
-    while((line= socket->readLine()) != "") {
+    while(validConnection && (line= socket->readLine()) != "") {
         vector<string> bodyParts;
         string data;
         Message request, response;
-        logger->log(Level::INFO, line);
+        logger->log(Level::INFO, "request: " + line);
         request= Message::parse(line);
 
         switch (request.getRequest()) {
             case Message::CONNECT:
+                if (request.getBody() != password) {
+                    validConnection= false;
+                    data= "Invalid password";
+                } else {
+                    data= "ok";
+                }
+                
                 break;
             case Message::RETRIEVE:
                 bodyParts= utility::split(request.getBody(), '.');
@@ -51,55 +111,13 @@ void handler(shared_ptr<Socket> socket) {
         socket->write(response.toString());
         logger->log(Level::INFO, "response: " + response.toString());
     }
+    socket->close();
+    logger->log(Level::INFO, "Connection to " + socket->getAddressPort() + " closed");
 }
 
-void initDbConnection(const string& dbURL, const string& user, const string& passwd) {
+void initDbConnection() {
     dbConn= new SqliteConnection();
-    dbConn->open(dbURL, user, passwd);
+    dbConn->open(dbURL, dbUser, dbPasswd);
 }
 
-int main(int argc, char **argv) {
-    logger= Logger::getLogger("saremotedatabase");
-    ServerSocket server;
-    int port;
-    string dbURL, dbUser, dbPasswd;
 
-    CliBuilder *cli= CliBuilder::getBuilder();
-    (*cli).addOption(Option("-tcpport", 0, 1, [&port](const Arguments &args) -> void {
-        port= args.asInteger(0);
-    }).withArgName("number").withDescription("TPC port to listen on for requests").withRequired(true))
-    .addOption(Option("-dburl", 0, 1, [&dbURL](const Arguments &args) -> void {
-        dbURL= args.asString(0);
-    }).withArgName("url").withDescription("URL to the database storing achievement information").withRequired(true))
-    .addOption(Option("-h", [&cli](const Arguments &args) -> void {
-        cli->displayUsage();
-    }).withLongOpt("--help").withDescription("Displays this help message and exits"))
-    .addOption(Option("-dbuser", 0, 1, [&dbUser](const Arguments &args) -> void {
-        dbUser= args.asString(0);
-    }).withArgName("user name").withDescription("Username for logging into the database"))
-    .addOption(Option("-dbpwd", 0, 1, [&dbPasswd](const Arguments &args) -> void {
-        dbPasswd= args.asString(0);
-    }).withArgName("password").withDescription("Password for logging into the database"))
-    .setUsage("saremotedatabase -database <name> -tcpport <number> [options]");
-    cli->parse(argc, argv);
-
-    logger->addHandler(new ConsoleHandler());
-    logger->addHandler(new FileHandler());
-    server.bind(port);
-    initDbConnection(dbURL, dbUser, dbPasswd);
-    {
-        stringstream msg;
-        msg << "Listening on port: " << port;
-        logger->log(Level::INFO, msg.str());
-    }
-    while(true) {
-        stringstream msg;
-        shared_ptr<Socket> client= server.accept();
-        msg << "Received connection from " << client->getAddress() << ":" << client->getPort();
-        logger->log(Level::INFO, msg.str());
-        thread th(handler, client);
-        th.detach();
-    }
-
-    return 0;
-}
