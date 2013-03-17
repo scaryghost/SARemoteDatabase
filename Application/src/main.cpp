7247f9565dfe3ea30050b7b2395477161b95e178
etsai <etsai2@gmail.com>
2013-03-17 15:54:26 -0700

    Reorganized command line options

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -21,38 +21,38 @@ int main(int argc, char **argv) {
         global::logger->addHandler(new ConsoleHandler());
 
         CliBuilder *cli= CliBuilder::getBuilder();
-        (*cli).addOption(Option("-tcpport", 0, 1, [&port](const Arguments &args) -> void {
+        (*cli).addOption(Option("--tcp-port", 0, 1, [&port](const Arguments &args) -> void {
             port= args.asInteger(0);
         }).withArgName("number").withDescription("TCP port to listen on for requests").withRequired(true))
-        .addOption(Option("-dburl", 0, 1, [&dbURL](const Arguments &args) -> void {
+        .addOption(Option("--data-url", 0, 1, [&dbURL](const Arguments &args) -> void {
             dbURL= args.asString(0);
-        }).withArgName("url").withDescription("URL to the database storing achievement information").withRequired(true))
-        .addOption(Option("-passwd", 0, 1, [&password](const Arguments &args) -> void {
+        }).withArgName("url").withDescription("URL to where the achievement data is stored").withRequired(true))
+        .addOption(Option("--server-pw", 0, 1, [&password](const Arguments &args) -> void {
             password= args.asString(0);
         }).withArgName("password").withDescription("Password for connecting to the server").withRequired(true))
         .addOption(Option("-h", [&cli](const Arguments &args) -> void {
             cli->displayUsage();
         }).withLongOpt("--help").withDescription("Displays this help message and exits"))
-        .addOption(Option("-dbuser", 0, 1, [&dbUser](const Arguments &args) -> void {
+        .addOption(Option("--data-user", 0, 1, [&dbUser](const Arguments &args) -> void {
             dbUser= args.asString(0);
-        }).withArgName("username").withDescription("Username for logging into the database"))
-        .addOption(Option("-dbpwd", 0, 1, [&dbPasswd](const Arguments &args) -> void {
+        }).withArgName("username").withDescription("Username to access achievement data"))
+        .addOption(Option("--data-pw", 0, 1, [&dbPasswd](const Arguments &args) -> void {
             dbPasswd= args.asString(0);
-        }).withArgName("password").withDescription("Password for logging into the database"))
-        .addOption(Option("-dblib", 0, 1, [&dbLib](const Arguments &args) -> void {
+        }).withArgName("password").withDescription("Password to access achievement data"))
+        .addOption(Option("--data-lib", 0, 1, [&dbLib](const Arguments &args) -> void {
             dbLib= args.asString(0);
-        }).withArgName("libname").withDescription("Custom dll/so to process data in place of sqlite"))
-        .addOption(Option("-timeout", 0, 1, [&timeout](const Arguments &args) -> void {
+        }).withArgName("libname").withDescription("Custom dll/so to process data instead of sqlite"))
+        .addOption(Option("--connect-timeout", 0, 1, [&timeout](const Arguments &args) -> void {
             timeout= args.asInteger(0);
-        }).withArgName("sec").withDescription("How long to wait (in sec) before closing idle connection"))
+        }).withArgName("sec").withDescription("Max wait time before closing an idle connection"))
         .addOption(Option("-v", [](const Arguments &args) -> void {
             std::cout << "saremotedatabase " << version << std::endl;
             std::cout << "git page: https://github.com/scaryghost/SARemoteDatabase" << std::endl;
         }).withLongOpt("--version").withDescription("Prints version and exits"))
-        .addOption(Option("-logdir", 0, 1, [&logDir](const Arguments &args) -> void {
+        .addOption(Option("--log-dir", 0, 1, [&logDir](const Arguments &args) -> void {
             logDir= args.asString(0);
         }).withArgName("dir").withDescription("Directory to store log files in"))
-        .setUsage("saremotedatabase -dburl <url> -tcpport <number> -passwd <password> [options]");
+        .setUsage("saremotedatabase --data-url <url> --tcp-port <number> --server-pw <password> [options]");
         cli->parse(argc, argv);
 
         global::logger->addHandler(new FileHandler(logDir));
