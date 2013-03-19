ec2b6e17346400d9055607909c0a95bee9fba431
etsai <etsai2@gmail.com>
2013-03-19 03:21:14 -0700

    Updated the headers with comments

--- Data/Achievements.h
+++ Data/Achievements.h
@@ -10,28 +10,56 @@ namespace saremotedatabase {
 using std::string;
 using std::vector;
 
+/**
+ * C++ equivalent of the UnrealScript AchievementPack class from ServerAchievements
+ * @author etsai
+ */
 class Achievements {
 public:
+    /** Represents the state of an achievement */
     struct Info {
-        int index;
-        int completed;
-        int progress;
+        int index;          ///< Achievement index
+        int completed;      ///< 0 if not yet completed
+        int progress;       ///< How much progress has been accumulated
     };
 
+    /**
+     * Default constructor
+     */
     Achievements();
+    /**
+     * Constructs an Achievements object reprsented by the serialized data
+     * @param   data    Serialized representation of the object
+     * @see deserialize
+     */
     Achievements(const string& data);
 
+    /**
+     * Inserts an achievement state
+     * @param   info    Achievement state to add
+     */
     void insert(const Info& info);
+    /**
+     * Deserialize the data and update the Achievements variables
+     * @param   data    Serialized representation of the object
+     */
     void deserialize(const string& data);
 
+    /**
+     * Convert the object into a string representation
+     * @return  Serialized form of the object
+     */
     string serialize() const;
+    /**
+     * Get the set of states for each achievement
+     * @return  States for each achievement
+     */
     const vector<Info>& getInfo() const;
 
-    static const char dataSeparator;
-    static const char infoSeparator;
+    static const char dataSeparator;        ///< Character that separates each achievement state
+    static const char infoSeparator;        ///< Character that separates the data within an achievement state
 private:
-    vector<Info> achvInfo;
-
+    vector<Info> achvInfo;                  ///< Set of achievement states
 };
 
 }   //namespace saremotedatabase
--- Data/DataChannel.h
+++ Data/DataChannel.h
@@ -16,11 +16,38 @@ namespace saremotedatabase {
 using std::runtime_error;
 using std::string;
 
+/**
+ * Provides a connection to the achievement data.  All implementations of this interface must be thread safe.
+ * @author etsai
+ */
 class DataChannel {
 public:
+    /**
+     * Open a connection to where the data is stored, using the provided credentials if login is needed
+     * @param   dataURL             URL of the data location
+     * @param   dataUser            Username for login credentials
+     * @param   dataPw              Password for login credentials
+     * @throw   runtimne_error      If a connection cannot be made with the given parameters
+     */
     virtual void open(const string& dataURL, const string& dataUser, const string& dataPw) throw(runtime_error)= 0;
+    /**
+     * Close the connection and deallocates resources
+     */
     virtual void close()= 0;
+    /**
+     * Retrieves the achievement data for the specified player and achievement pack
+     * @param   steamid64           Player's steamid64 to lookup
+     * @param   packName            Achievement pack name to lookup
+     * @throw   runtime_error       If an error occured during the retrieval
+     */
     virtual Achievements retrieveAchievementData(const string& steamid64, const string& packName) throw(runtime_error)= 0;
+    /**
+     * Save the achievement data
+     * @param   steamid64           Owner's steamid64
+     * @param   packName            Achievement pack name
+     * @param   achvData            Achievement data to store
+     * @throw   runtime_error       If the data was unable to be written
+     */
     virtual void saveAchievementData(const string& steamid64, const string& packName, 
             const Achievements achvData) throw(runtime_error)= 0;
 };
